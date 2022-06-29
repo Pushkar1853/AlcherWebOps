0024ab2dc7f84fa104e42ad972e405cb4de6ce5a
Pushkar Ambastha <105978655+Pushkar1853@users.noreply.github.com>
2022-06-29 17:28:32 +0530

    Added Script

---
+++ script.js
@@ -0,0 +1,8 @@
+function myfunction1() {
+  document.getElementById("Change").innerHTML = "You've clicked button-1";
+  document.getElementById("Change2").innerHTML = "";
+}
+function myfunction2() {
+  document.getElementById("Change").innerHTML = "You've clicked button-2";
+  document.getElementById("Change2").innerHTML = "";
+}
